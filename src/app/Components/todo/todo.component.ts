8458befcaba49811b9536843ac1157ffb3ec9882
Yevhenii <80651146+YevhenSalamakha@users.noreply.github.com>
2022-06-30 10:50:58 +0300

    created header and form in layout

---
+++ src/app/Components/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss'],
+})
+export class TodoComponent implements OnInit {
+	
+  constructor() {}
+
+  ngOnInit(): void {}
+}
