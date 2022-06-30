a5d6cb60d3e6f5dba31972805bb6dcbcfa65cfa5
Yevhenii <80651146+YevhenSalamakha@users.noreply.github.com>
2022-06-30 12:41:26 +0300

    new function to add tasks to list of tasks

--- src/app/Components/todo/todo.component.ts
+++ src/app/Components/todo/todo.component.ts
@@ -1,5 +1,11 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Itask } from 'src/app/model/task';
 
 @Component({
   selector: 'app-todo',
@@ -8,12 +14,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class TodoComponent implements OnInit {
   todoForm!: FormGroup;
-
+  tasks: Itask[] = [];
+  inprogress: Itask[] = [];
+  done: Itask[] = [];
   constructor(private formbuilder: FormBuilder) {}
 
   ngOnInit(): void {
-	this.todoForm = this.formbuilder.group({
-		item: ['', Validators.required]
-	})
+    this.todoForm = this.formbuilder.group({
+      item: ['', Validators.required],
+    });
+  }
+
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false,
+    });
+  }
+
+  drop(event: CdkDragDrop<Itask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, TodoComponent],
@@ -27,7 +28,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-	MatButtonModule
+	MatButtonModule,
+	DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent],
